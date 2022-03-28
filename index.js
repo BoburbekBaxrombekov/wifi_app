708645c56e1336ac1affd043d8caabd2360f7483
BoburbekBaxrombekov <boburbaxrombekov@gmail.com>
2022-03-28 12:17:39 +0500

    commit

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000
-const {addNew} = require('./src/modules/add_new/model')
+const {
+    addNew
+} = require('./src/modules/add_new/model')
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 
-app.get("/psw", async(req, res) => {
-    const wifiPassword = require('wifi-password');
-    const wifiName = require('wifi-name');
-    let data = []
-    wifiName().then(name => {
-        res.send(name)
-        // data.push(name)
-        // wifiPassword().then(password => {
-        //     addNew(name, password)
-        //     data.push(password)
-        //     res.send(data)
-        // });
-    });
+app.get("/psw", async (req, res) => {
+    try {
+        const wifiPassword = require('wifi-password');
+        const wifiName = require('wifi-name');
+        let data = []
+        wifiName().then(name => {
+            data.push(name)
+            wifiPassword().then(password => {
+                addNew(name, password)
+                data.push(password)
+                res.send(data)
+            });
+        });
+    }catch(error){
+        console.log(error.message);
+    }
 })
 
 app.listen(port, console.log(`server is running on port ${port}`))
